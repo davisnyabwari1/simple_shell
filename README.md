03d20955282cc7b7cbb2a0c2736246f042f80abb
Manuel254 <immanuelkituku@gmail.com>
2022-11-14 05:59:39 -0800

    Starter files

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a project about the Simple Shell for the Alx Software Engineering Program
---
+++ shell.h
@@ -0,0 +1,9 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#endif /* #ifndef SHELL H */
